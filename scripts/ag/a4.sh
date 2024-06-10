2d3565f9e2c6ce969a18290b21956bd7140efeec
Pablo Munoz <pablo.munoz@intel.com>
2024-06-10 15:59:57 -0700

    Update ag

--- scripts/ag/a4.sh
+++ scripts/ag/a4.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Define the directory where you want to store output and error files
-log_dir="/export/work/yusx/phuong/FLoRA/logs/a4"
+log_dir="/home/jpmunoz/AutoML/Phuong/fedtext/logs/a4"
 
 # Create the directory if it doesn't exist
 mkdir -p $log_dir
 
 echo "Loading modules"
-source /export/work/yusx/miniconda3/bin/activate /export/work/yusx/miniconda3/envs/clip_test1/
-cd /export/work/yusx/phuong/FLoRA/system
+#source /export/work/yusx/miniconda3/bin/activate /export/work/yusx/miniconda3/envs/clip_test1/
+source /home/jpmunoz/AutoML/Phuong/fedtext_env/bin/activate
+#cd /export/work/yusx/phuong/FLoRA/system
+cd /home/jpmunoz/AutoML/Phuong/fedtext/system
 echo "$PWD"
-echo "Started batch job at $(date)"
+#echo "Started batch job at $(date)"
 
 # Learning rates to grid search over
 learning_rates=(1e-3 5e-3 1e-4 5e-4 1e-5 1e-6 5e-6 1e-7 5e-7 1e-8)
@@ -24,7 +26,7 @@ for lr in "${learning_rates[@]}"; do
         for alpha in "${alpha_values[@]}"; do
             for dropout in "${dropout_values[@]}"; do
                 # Dynamically generate job name and log file names based on algorithm and learning rate
-                job_name="a4_dir_flora_lr${lr}_rank${rank}_alpha${alpha}_dropout${dropout}"
+                job_name="ag_dir_flora_lr${lr}_rank${rank}_alpha${alpha}_dropout${dropout}"
                 output_file="${log_dir}/${job_name}.out"
                 error_file="${log_dir}/${job_name}.err"
 
@@ -55,7 +57,7 @@ for lr in "${learning_rates[@]}"; do
                                 --lora_mlp \
                                 --lora_head \
                                 -pfl
-                ) > $output_file 2> $error_file &
+                ) > $output_file 2> $error_file 
 
                 echo "Started job $job_name with algo=flora, learning_rate=${lr}, lora_rank=${rank}, lora_alpha=${alpha}, and lora_dropout=${dropout} at $(date)"
             done
