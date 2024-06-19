870987a9f4ec58fda522df75e713669de68e74c6
dphuongn <nguyenduyphuong94@gmail.com>
2024-06-18 19:18:16 -0500

    algo

--- scripts/cola/c2_fft.sh
+++ scripts/cola/c2_fft.sh
@@ -13,18 +13,21 @@ cd /home/jpmunoz/AutoML/Phuong/fedtext/system
 echo "$PWD"
 #echo "Started batch job at $(date)"
 
+# Algorithm to run
+algo='fedfft'
+
 # Learning rates to grid search over
 learning_rates=(1e-3 5e-3 1e-4 5e-4 1e-5 5e-5 1e-6 5e-6 1e-7 5e-7 1e-8 5e-8)
 
 # Iterate over learning rates, rank values, alpha values, and dropout values
 for lr in "${learning_rates[@]}"; do
     # Dynamically generate job name and log file names based on algorithm and learning rate
-    job_name="c2_dir10_fft_lr${lr}"
+    job_name="c2_dir10_${algo}_lr${lr}"
     output_file="${log_dir}/${job_name}.out"
     error_file="${log_dir}/${job_name}.err"
 
     echo "$PWD"
-    echo "Running with algo=fedfft, learning_rate=${lr}"
+    echo "Running with algo=${algo}, learning_rate=${lr}"
 
     # Run the job directly
     (
@@ -43,7 +46,7 @@ for lr in "${learning_rates[@]}"; do
                     -pfl
     ) > $output_file 2> $error_file 
 
-    echo "Started job $job_name with algo=fedfft, learning_rate=${lr} at $(date)"
+    echo "Started job $job_name with algo=${algo}, learning_rate=${lr} at $(date)"
 done
 
 echo "Finish starting all jobs at $(date)"
